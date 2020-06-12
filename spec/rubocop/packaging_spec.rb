cf6ee30004da4b3dcdc667612c24827e4a4debf1
Antonio Terceiro <asa@terceiro.xyz>
2020-06-11 22:14:40 -0300

    spec: remove useless test

--- spec/rubocop/packaging_spec.rb
+++ spec/rubocop/packaging_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RuboCop::Packaging do
   it 'has a version number' do
     expect(RuboCop::Packaging::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(true).to eq(true)
-  end
 end
