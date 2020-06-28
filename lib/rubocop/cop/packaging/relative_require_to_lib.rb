b622d9ccf4e6577760d077a2ef50f08fd9de60fd
Utkarsh Gupta <utkarsh@debian.org>
2020-06-29 02:18:22 +0530

    Split require and require_relative cops into two
    Previously, both `require` and `require_relative` calls
    were being inspected by the same cop.
    This had various problems as catching all `require` calls
    can be a little tricky and not as straight-forward as
    `require_relative` ones. Hence, the split.
    
    For now, the root directory is hardcoded.
    And the specs of `require` calls have been filtered out.
    Moreover, AST pattern has been fixed to just accomodate
    the `require_relative` calls.
    
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- lib/rubocop/cop/packaging/relative_require_to_lib.rb
+++ lib/rubocop/cop/packaging/relative_require_to_lib.rb
@@ -13,16 +13,20 @@ module RuboCop # :nodoc:
         # offense of using `require` with relative path to lib.
         MSG = 'Avoid using `require` with relative path to lib.'
 
-        def_node_matcher :require?, <<~PATTERN
-          {(send nil? {:require :require_relative} (str #starts_with_relative_path?))
-           (send nil? {:require :require_relative} (send (const nil? :File) :expand_path (str #starts_with_relative_path?) {(str _) (send nil? _)}))
-           (send nil? {:require :require_relative} (send (send (const nil? :File) :dirname {(str _) (send nil? _)}) :+ (str #starts_with_relative_path?)))}
+        def_node_matcher :require_relative, <<~PATTERN
+          (send nil? :require_relative
+            (str #starts_with_relative_path?))
         PATTERN
+
+        def investigate(processed_source)
+          @file_path = processed_source.file_path
+          @file_directory = File.dirname(@file_path)
+        end
 
         # Extended method.
         # TODO: document it.
         def on_send(node)
-          return unless require?(node)
+          return unless require_relative(node)
 
           add_offense(node)
         end
@@ -30,7 +34,16 @@ module RuboCop # :nodoc:
         # This method is called from inside `#def_node_search`.
         # It is used to find the strings that uses relative path.
         def starts_with_relative_path?(str)
-          str.match?(%r{.*\./lib/})
+          root_dir = get_root
+          relative_dir = File.expand_path(str, @file_directory)
+          relative_dir.delete_prefix!(root_dir + '/')
+          relative_dir.start_with?('lib')
+          # str.match?(%r{.*\./lib/})
+        end
+
+        def get_root(*)
+          # FIXME
+          '/some'
         end
       end
     end
--- spec/rubocop/cop/packaging/relative_require_to_lib_spec.rb
+++ spec/rubocop/cop/packaging/relative_require_to_lib_spec.rb
@@ -7,145 +7,98 @@ RSpec.describe RuboCop::Cop::Packaging::RelativeRequireToLib do
 
   let(:message) { RuboCop::Cop::Packaging::RelativeRequireToLib::MSG }
 
-  let(:path) { './../..' }
+  let(:processed_source) { parse_source(source) }
 
-  it 'registers an offense when using `require` with relative path to rubocop' do
-    expect_offense(<<~RUBY)
-      require '../lib/rubocop'
-      ^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
+  before do
+    allow(processed_source.buffer)
+      .to receive(:name).and_return(filename)
+    _investigate(cop, processed_source)
   end
 
-  it 'registers an offense when using `require_relative` with relative path to rubocop' do
-    expect_offense(<<~RUBY)
-      require_relative '../lib/rubocop'
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
+  context 'when `require_relative` call lies outside spec/' do
+    let(:filename) { '/some/spec/foo_spec.rb' }
+    let(:source) { 'require_relative "../lib/foo.rb"' }
+
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        #{source}
+        #{'^' * source.length} #{message}
+      RUBY
+    end
   end
 
-  it 'registers an offense when using `require` with relative path twice' do
-    expect_offense(<<~RUBY)
-      require '../lib/rubocop'
-      ^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-      require '../lib/rubocop/file'
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
+  context 'when `require_relative` call with nested path lies outside spec/' do
+    let(:filename) { '/some/spec/rubocop/cop/foo_spec.rb' }
+    let(:source) { 'require_relative "../../../lib/foo.rb"' }
+
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        #{source}
+        #{'^' * source.length} #{message}
+      RUBY
+    end
   end
 
-  it 'registers an offense when using `require_relative` with relative path twice' do
-    expect_offense(<<~RUBY)
-      require_relative '../lib/rubocop'
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
+  context 'when one `require_relative` call lies outside spec/' do
+    let(:filename) { '/some/spec/foo_spec.rb' }
+    let(:source) { <<~RUBY.chomp }
+      require_relative 'spec_helper'
       require_relative '../lib/rubocop/file'
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
     RUBY
+
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        #{source}
+        #{'^' * 38} #{message}
+      RUBY
+    end
   end
 
-  it 'registers an offense when using `require` with (more) relative path to rubocop' do
-    expect_offense(<<~RUBY)
-      require '../../lib/rubocop'
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
-  end
-
-  it 'registers an offense when using `require_relative` with (more) relative path to rubocop' do
-    expect_offense(<<~RUBY)
-      require_relative '../../lib/rubocop'
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
-  end
-
-  it 'registers an offense when using `unshift` and `require` with relative path to file.rb' do
-    expect_offense(<<~RUBY)
+  context 'when `require_relative` call with `unshift` lies outside spec/' do
+    let(:filename) { '/some/spec/foo_spec.rb' }
+    let(:source) { <<~RUBY.chomp }
       $:.unshift('../lib')
-      require '../lib/file.rb'
-      ^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
+      require_relative "../lib/file"
     RUBY
+
+    it 'registers an offense' do
+      expect_offense(<<~RUBY)
+        #{source}
+        #{'^' * 30} #{message}
+      RUBY
+    end
   end
 
-  it 'registers an offense when using `unshift` and `require_relative` with relative path to file.rb' do
-    expect_offense(<<~RUBY)
-      $:.unshift('../lib')
-      require_relative '../lib/file.rb'
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
+  context 'when the `require_relative` call to `lib` lies inside spec/' do
+    let(:filename) { '/some/specs/rubocop/cop/foo_spec.rb' }
+    let(:source) { 'require_relative "../lib/foo.rb"' }
+
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        #{source}
+      RUBY
+    end
   end
 
-  it 'registers an offense when using `require` with a variable and a relative path' do
-    expect_offense(<<~RUBY)
-      require "#{path}/../lib/rubocop"
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
+  context 'when the `require_relative` call lies inside spec/' do
+    let(:filename) { '/some/specs/rubocop/cop/foo_spec.rb' }
+    let(:source) { 'require_relative "../foo.rb"' }
+
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        #{source}
+      RUBY
+    end
   end
 
-  it 'registers an offense when using `require_relative` with a variable and a relative path' do
-    expect_offense(<<~RUBY)
-      require_relative "#{path}/../lib/rubocop"
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
-  end
+  context 'when the `require_relative` call lies inside spec/' do
+    let(:filename) { '/some/specs/foo_spec.rb' }
+    let(:source) { 'require_relative "spec/rubocop/bar"' }
 
-  it 'registers an offense when using `require` with `File.expand_path` and `__FILE__`' do
-    expect_offense(<<~RUBY)
-      require File.expand_path('../lib/rubocop', __FILE__)
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
-  end
-
-  it 'registers an offense when using `require` with `File.expand_path` and `__dir__`' do
-    expect_offense(<<~RUBY)
-      require File.expand_path('../lib/rubocop', __dir__)
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
-  end
-
-  it 'registers an offense when using `require` with `File.dirname` and `__FILE__`' do
-    expect_offense(<<~RUBY)
-      require File.dirname(__FILE__) + '/../lib/rubocop'
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
-  end
-
-  it 'registers an offense when using `require` with `File.dirname` and `__dir__`' do
-    expect_offense(<<~RUBY)
-      require File.dirname(__dir__) + '/../lib/rubocop'
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ #{message}
-    RUBY
-  end
-
-  it 'does not register an offense when using `require` with absoulte path to file' do
-    expect_no_offenses(<<~RUBY)
-      require 'file'
-    RUBY
-  end
-
-  it 'does not register an offense when using `require_relative` with absoulte path to file' do
-    expect_no_offenses(<<~RUBY)
-      require_relative 'file'
-    RUBY
-  end
-
-  it 'does not register an offense when using `require` with absolute path to spec dir' do
-    expect_no_offenses(<<~RUBY)
-      require 'spec/rubocop/file'
-    RUBY
-  end
-
-  it 'does not register an offense when using `require_relative` with absolute path to spec dir' do
-    expect_no_offenses(<<~RUBY)
-      require_relative 'spec/rubocop/file'
-    RUBY
-  end
-
-  it 'does not register an offense when using `require` with relative path to !lib' do
-    expect_no_offenses(<<~RUBY)
-      require '../spec/rubocop/file'
-    RUBY
-  end
-
-  it 'does not register an offense when using `require_relative` with relative path to !lib' do
-    expect_no_offenses(<<~RUBY)
-      require_relative '../spec/rubocop/file'
-    RUBY
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        #{source}
+      RUBY
+    end
   end
 end
