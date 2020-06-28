ddae67703c6fdd1112039044a795068b1b8afcac
Utkarsh Gupta <utkarsh@debian.org>
2020-06-29 02:57:36 +0530

    Diversify examples/tests
    The tests were written wrt a single directory, "spec" and
    a single file, "foo".
    This diversifies these tests to include all possible
    directories like "spec", "specs", "test", and "tests" and
    all sorts of files like "foo", "bar", "baz", and "qux".
    
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- spec/rubocop/cop/packaging/relative_require_to_lib_spec.rb
+++ spec/rubocop/cop/packaging/relative_require_to_lib_spec.rb
@@ -27,9 +27,9 @@ RSpec.describe RuboCop::Cop::Packaging::RelativeRequireToLib do
     end
   end
 
-  context 'when `require_relative` call with nested path lies outside spec/' do
-    let(:filename) { '/some/spec/rubocop/cop/foo_spec.rb' }
-    let(:source) { 'require_relative "../../../lib/foo.rb"' }
+  context 'when `require_relative` call with nested path lies outside test/' do
+    let(:filename) { '/some/test/rubocop/cop/bar_spec.rb' }
+    let(:source) { 'require_relative "../../../lib/bar"' }
 
     it 'registers an offense' do
       expect_offense(<<~RUBY)
@@ -39,39 +39,39 @@ RSpec.describe RuboCop::Cop::Packaging::RelativeRequireToLib do
     end
   end
 
-  context 'when one `require_relative` call lies outside spec/' do
-    let(:filename) { '/some/spec/foo_spec.rb' }
+  context 'when one `require_relative` call lies outside specs/' do
+    let(:filename) { '/some/specs/baz_spec.rb' }
     let(:source) { <<~RUBY.chomp }
       require_relative 'spec_helper'
-      require_relative '../lib/rubocop/file'
+      require_relative '../lib/rubocop/baz'
     RUBY
 
     it 'registers an offense' do
       expect_offense(<<~RUBY)
         #{source}
-        #{'^' * 38} #{message}
+        #{'^' * 37} #{message}
       RUBY
     end
   end
 
-  context 'when `require_relative` call with `unshift` lies outside spec/' do
-    let(:filename) { '/some/spec/foo_spec.rb' }
+  context 'when `require_relative` call with `unshift` lies outside tests/' do
+    let(:filename) { '/some/tests/qux_spec.rb' }
     let(:source) { <<~RUBY.chomp }
       $:.unshift('../lib')
-      require_relative "../lib/file"
+      require_relative "../lib/qux"
     RUBY
 
     it 'registers an offense' do
       expect_offense(<<~RUBY)
         #{source}
-        #{'^' * 30} #{message}
+        #{'^' * 29} #{message}
       RUBY
     end
   end
 
   context 'when the `require_relative` call to `lib` lies inside spec/' do
-    let(:filename) { '/some/specs/rubocop/cop/foo_spec.rb' }
-    let(:source) { 'require_relative "../lib/foo.rb"' }
+    let(:filename) { '/some/spec/rubocop/cop/foo_spec.rb' }
+    let(:source) { 'require_relative "../lib/foo"' }
 
     it 'does not register an offense' do
       expect_no_offenses(<<~RUBY)
@@ -80,9 +80,9 @@ RSpec.describe RuboCop::Cop::Packaging::RelativeRequireToLib do
     end
   end
 
-  context 'when the `require_relative` call lies inside spec/' do
-    let(:filename) { '/some/specs/rubocop/cop/foo_spec.rb' }
-    let(:source) { 'require_relative "../foo.rb"' }
+  context 'when the `require_relative` call lies inside tests/' do
+    let(:filename) { '/some/tests/rubocop/cop/bar_spec.rb' }
+    let(:source) { 'require_relative "../bar"' }
 
     it 'does not register an offense' do
       expect_no_offenses(<<~RUBY)
@@ -91,9 +91,9 @@ RSpec.describe RuboCop::Cop::Packaging::RelativeRequireToLib do
     end
   end
 
-  context 'when the `require_relative` call lies inside spec/' do
-    let(:filename) { '/some/specs/foo_spec.rb' }
-    let(:source) { 'require_relative "spec/rubocop/bar"' }
+  context 'when the `require_relative` call lies inside test/' do
+    let(:filename) { '/some/test/qux_spec.rb' }
+    let(:source) { 'require_relative "spec/rubocop/qux.rb"' }
 
     it 'does not register an offense' do
       expect_no_offenses(<<~RUBY)
